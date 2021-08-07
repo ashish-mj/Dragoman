258edf4ea36e94ee2054eb3c6a90e1f6cd72fbfa
Ashish M J <ashm.jagadeesh@gmail.com>
2021-08-07 19:58:23 +0530

    Add styling to home

--- dragoman/urls.py
+++ dragoman/urls.py
@@ -2,9 +2,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.hello),
     url(r'^translate/', views.translate,name='translate'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
